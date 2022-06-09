84a557eab5ff7a72df00b11fb65c7ba00ca4b79f
Omer Husain <husain.omer@gmail.com>
2022-06-09 00:00:28 -0400

    Refactored routes and help functions

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const videoRoutes = require("./routes/videos");
-
 const port = 8080;
 
+
+app.use(express.json());
+
 app.use("/videos", videoRoutes);
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
--- routes/videos.js
+++ routes/videos.js
@@ -1,16 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+const filePath = "./data/videos.json";
+
+//helper functions
+
+//get data from json file, parse it and  return as array of objects
+const getVideosFromJSON = () => JSON.parse(fs.readFileSync(filePath));
+
+//filters through array of objects to find specifc object based on user supplied id and returns it
+const getVideoById = (videoId) => {
+  let videos = getVideosFromJSON();
+  let selectedVideo = videos.filter((video) => video.id === videoId);
+  return selectedVideo.length > 0 ? selectedVideo[0] : {};
+};
+
+const writeNewVideosToJSON = (video) => {
+  let videos = getVideosFromJSON();
+  videos.push(video);
+  let updatedVideos = JSON.stringify(videos);
+  fs.writeFileSync(filePath, updatedVideos);
+};
+
+//routes
 
 router.get("/:id", (req, res) => {
-  res.send("GET with ID");
+  res.json(getVideoById(req.params.id));
 });
 
 router.get("/", (req, res) => {
-  res.send("GET method");
+  res.json(getVideosFromJSON());
 });
 
 router.post("/", (req, res) => {
-  res.send("POST method");
+  let uploadedVideo = {
+    title: req.body.title,
+    channel: "Sample Channel",
+    image: "Image path",
+    description: req.body.description,
+    views: "1000",
+    likes: "250,000",
+    duration: "3:50",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: Date(),
+    comments: [],
+    id: uuidv4(),
+  };
+
+  writeNewVideosToJSON(uploadedVideo);
+  res.json(getVideosFromJSON());
 });
 
 module.exports = router;
